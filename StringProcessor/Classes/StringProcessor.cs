dff3882515e0e0829de0fbffaf4f3074794729ab
Vasily Himchenko <White_Scorpion@inbox.lv>
2022-05-07 23:20:12 +0300

    New project about string processing. May be even works. Didn't implemented writing to file :)

---
+++ StringProcessor/Classes/StringProcessor.cs
@@ -0,0 +1,124 @@
+﻿namespace StringProcessor.Classes
+{
+    internal enum CheckResult
+    {
+        Exit,
+        Result,
+        Clean,
+        CheckFormat,
+        ValidationFailedNextStepRequest,
+        Continue
+    }
+
+    internal enum CheckFormat
+    {
+        Failed,
+        Succeeded
+    }
+
+    internal class StringProcessor
+    {
+        private readonly List<string> _result;
+        private readonly List<string> _resultBad; // написано было - запихивать в "плохие строки", но что потом сэтим делать - нипанятна. Вообщем я их чищу по Clean тоже и они есть - если надо используй где надо.
+        internal const int FormatMin = 1;
+        internal const int FormatMax = 7;
+
+        // Строка для вывода результата - она НЕ содержит данных до момента, пока этот результат надо выводить.
+        // настоящий источник данных и хранилище всех валидных сегментов - _result, откуда все сегменты и джойнятся в результат
+        public string Result => string.Join(',', this._result); 
+
+        internal StringProcessor()
+        {
+            this._result = new List<string>();
+            this._resultBad = new List<string>();
+        }
+
+        public CheckResult CheckInput(string? line)
+        {
+            if (string.IsNullOrEmpty(line))
+                return CheckResult.Exit;
+
+            if (line.ToLower().Equals("result", StringComparison.OrdinalIgnoreCase))
+                return CheckResult.Result;
+
+            if (line.ToLower().Equals("clean", StringComparison.OrdinalIgnoreCase))
+                return CheckResult.Clean;
+
+            return line.Length switch
+            {
+                >= FormatMax => CheckResult.CheckFormat,
+                >= FormatMin and < FormatMax => CheckResult.ValidationFailedNextStepRequest,
+                _ => CheckResult.Continue
+            };
+        }
+
+        public void Clean()
+        {
+            this._result.Clear();
+            this._resultBad.Clear();
+        }
+
+        public CheckFormat CheckFormat(string s)
+        {
+            if (MatchFormat(s))
+            {
+                if (this._result.Count == 0)
+                    this._result.Add(s);
+                else
+                {
+                    var fragmentIndex = -1;
+                    for (var i = 0; i < this._result.Count; i++)
+                    {
+                        if (this._result[i][0] < s[0])
+                            fragmentIndex = i;
+                        else if (this._result[i][0] == s[0])
+                        {
+                            var stringFragmentNumbers = GetNumbers(this._result[i]);
+                            var fragmentNumbers = GetNumbers(s);
+
+                            if (stringFragmentNumbers > fragmentNumbers)
+                                fragmentIndex = i;
+                            else if (stringFragmentNumbers == fragmentNumbers)
+                            {
+                                var stringFragment3Rd = GetLetter(this._result[i], 2);
+                                var fragment3Rd = GetLetter(s, 2);
+                                if (stringFragment3Rd <= fragment3Rd)
+                                    fragmentIndex = i;
+                            }
+                        }
+                    }
+
+                    if (fragmentIndex >= 0)
+                        this._result.Insert(fragmentIndex, s);
+                }
+
+                return Classes.CheckFormat.Succeeded;
+            }
+
+            this._resultBad.Add(s); // add to failed rows
+            return Classes.CheckFormat.Failed;
+        }
+
+        private static char GetLetter(string input, int index)
+        {
+            return input[index];
+        }
+
+        private static int GetNumbers(string input)
+        {
+            var stringResult = string.Empty;
+            foreach (var item in input)
+            {
+                if (char.IsNumber(item))
+                    stringResult += item;
+            }
+
+            return string.IsNullOrEmpty(stringResult) ? 0 : Convert.ToInt32(stringResult);
+        }
+
+        private static bool MatchFormat(string s)
+        {
+            return s.Length == 7 && char.IsLetter(s[0]) && char.IsDigit(s[1]) && char.IsDigit(s[2]) && char.IsDigit(s[3]) && char.IsDigit(s[4]) && char.IsLetter(s[5]) && char.IsLetter(s[6]);
+        }
+    }
+}
---
+++ StringProcessor/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using StringProcessor.Classes;
+
+Console.WriteLine("Hello, StringProcessor is here!");
+Console.WriteLine("Started work.");
+
+const string inputRules = "Rules";
+Console.WriteLine(inputRules);
+
+var processor = new StringProcessor.Classes.StringProcessor();
+
+var readLine = InputStringsOffer();
+
+var continueWork = true;
+
+while (continueWork && readLine != null)
+{
+    var splitStrings = readLine.Split(',');
+    var checkResult = CheckResult.Continue;
+
+    foreach (var s in splitStrings)
+    {
+        checkResult = processor.CheckInput(s);
+        switch (checkResult)
+        {
+            case CheckResult.Exit:
+                continueWork = false;
+                break;
+            case CheckResult.Result:
+            {
+                Console.WriteLine($"Result string: {processor.Result}");
+            }
+                break;
+            case CheckResult.Clean:
+            {
+                processor.Clean();
+                Console.Write(inputRules);
+            }
+                break;
+            case CheckResult.CheckFormat:
+            {
+                switch (processor.CheckFormat(s))
+                {
+                    case CheckFormat.Succeeded:
+                        Console.WriteLine($"Format check - succeeded. String '{s}' added to result.");
+                        break;
+                    case CheckFormat.Failed:
+                    default:
+                        Console.WriteLine($"Format check - failed. Cannot add string '{s}' to result.");
+                        break;
+                }
+            }
+                break;
+            case CheckResult.ValidationFailedNextStepRequest:
+                Console.WriteLine($"Validation check - failed: string '{s}' length in range of [{StringProcessor.Classes.StringProcessor.FormatMin}..{StringProcessor.Classes.StringProcessor.FormatMax}]");
+                break;
+            case CheckResult.Continue:
+            default: break;
+        }
+    }
+
+    if (checkResult == CheckResult.Exit)
+        continueWork = false;
+    else
+        readLine = InputStringsOffer();
+}
+
+Console.WriteLine("Finished work.");
+
+static string? InputStringsOffer()
+{
+    Console.Write("Input string (or strings set with ',' character as delimiter): ");
+    var inputLine = Console.ReadLine();
+    return inputLine;
+}
